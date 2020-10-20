cca7da97d770c4d2e444ac6fefadce33f41cfe47
alina <alinaclit@yahoo.com>
2020-10-20 19:26:13 +0300

    fix bug: spinner for categories

--- app/src/main/java/com/example/buco/NewReceipt.java
+++ app/src/main/java/com/example/buco/NewReceipt.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class NewReceipt extends Fragment {
+public class NewReceipt extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    String[] categories = { "Groceries", "Bills", "Drugs", "Fashion", "Beauty", "Night Out" };
 
     @Override
     public View onCreateView(
@@ -30,5 +36,20 @@ public class NewReceipt extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        Spinner spin = getView().findViewById(R.id.category);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getContext(), "Selected User: "+ categories[position] ,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
     }
 }
